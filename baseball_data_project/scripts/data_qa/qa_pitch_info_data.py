6723a69f8d176f5cfbf652a906f0da8e757f24ab
Colin Clapham <colinclapham18@gmail.com>
2024-04-13 10:30:14 -0400

    rename file path

--- baseball_data_project/scripts/data_qa/qa_pitch_info_data.py
+++ baseball_data_project/scripts/data_qa/qa_pitch_info_data.py
@@ -12,7 +12,7 @@ for i in game_log_years:
     # team_acronyms = [['ANA', 'A']]
     for j in team_acronyms:
         # Specify the path to your Parquet file
-        parquet_file_path = f'../deliverables/pitch_info/{i}/{j[0]}{i}_pitch_info_data.parquet'
+        parquet_file_path = f'/Users/colinclapham/github/baseball-data-project/baseball_data_project/deliverables/pitch_info/{i}/{j[0]}{i}_pitch_info_data.parquet'
 
         # Read the Parquet file into a DataFrame
         pitch_info_df = pd.read_parquet(parquet_file_path)
